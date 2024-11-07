39e9382c148dbfff2a4358a114688b257db1a667
Ahmed Al-kaf <ahmedalkaff@yahoo.com>
2024-11-07 09:45:56 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_FcOQjz0L0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3l68851kpd3nhtbafg10v5k0o3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
