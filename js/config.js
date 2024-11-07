90574cf11678237d2f8001eb1aa3fc03ff55d9dd
Ahmed Al-kaf <ahmedalkaff@yahoo.com>
2024-11-07 09:54:04 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_FcOQjz0L0', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3l68851kpd3nhtbafg10v5k0o3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_hJs4TWDkM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '17mk584sv5fvsrlptl7mq7587g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
